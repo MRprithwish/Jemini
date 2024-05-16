0f4e77812b8c62a0af94151e4382f5ec522cf00f
MRprithwish <majumderprithwish73@gmail.com>
2024-05-16 10:20:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './component/Sidebar/Sidebar'
+import Main from './component/main/Main';
+
+function App() {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
